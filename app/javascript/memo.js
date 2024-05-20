b0fd7a0729a32ea24f436711ed3d9cfd5ba01361
kikusachi <168533677+kikusachi@users.noreply.github.com>
2024-05-20 14:50:49 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,17 @@
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+  return html;
+};
+
 function post (){
   //リクエストを送信する処理
   //console.log("イベント発火");
@@ -6,9 +20,31 @@ function post (){
     e.preventDefault();
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
-    XHR.open("POST", "/posts", true);
+    XHR.open("POST", "/posts", true);  //「posts」←「post」にする
     XHR.responseType = "json";
     XHR.send(formData);
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      };
+      const list = document.getElementById("list");
+      const formText = document.getElementById("content");
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      console.log(formText.value);
+      const item = XHR.response.post;
+      const html = `
+        <div class="post">
+          <div class="post-date">
+            投稿日時：${item.created_at}
+          </div>
+          <div class="post-content">
+            ${item.content}
+          </div>
+        </div>`;
+        list.insertAdjacentHTML("afterend", html);
+        formText.value = "";
+    };
   });
  };
  
